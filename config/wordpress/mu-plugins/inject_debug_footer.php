f7fddfc12c150657c33f6765b5cb566bb49dc7f2
David Laing <david@davidlaing.com>
2013-05-05 22:41:39 +0100

    Add debug_footer with "Launch debugger" link

---
+++ config/wordpress/mu-plugins/inject_debug_footer.php
@@ -0,0 +1,32 @@
+<?php
+function sbw_inject_debug_footer() {
+?>
+<style>
+	#debug_footer {
+	   position:fixed;
+	   left:0px;
+	   bottom:0px;
+	   height:50px;
+	   width:100%;
+	   background:#999;
+	   padding:10px;
+	}
+	#debug_footer .inner {
+	  background: #FFF;
+		width: 98%;
+		text-align: center;
+		padding: 5px;
+	}
+</style>
+<div id="debug_footer">
+	<div class="inner">
+		<a href='http://localhost:9222' target="_blank" 
+			 onclick="setTimeout(function() { window.location.reload(); },1000);">
+			 Launch debugger (Chrome only)
+		</a>
+	</div>
+</div>
+<?php
+}
+
+add_action( 'shutdown', 'sbw_inject_debug_footer' );
